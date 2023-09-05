241d9f99d775d5b60bf2e8515e7c054be78e164a
Marco Aurelio Molinaro <marco.molinaro@gft.com>
2023-09-05 15:44:45 -0300

    Criação do projeto - primeiro commit

---
+++ src/main/java/br/com/ovd/kafka/MyLivenessCheck.java
@@ -0,0 +1,15 @@
+package br.com.ovd.kafka;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class MyLivenessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("alive");
+    }
+
+}
