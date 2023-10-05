1737e28f613809dfb9ee090690622e862d4c41f4
Marco Aurelio Molinaro <marco.molinaro@gft.com>
2023-10-05 17:14:04 -0300

    SearchEngineIntegrationProcessor método processar

--- src/main/java/br/com/ovd/kafka/integration/model/source/ProductSite.java
+++ src/main/java/br/com/ovd/kafka/integration/model/source/ProductSite.java
@@ -1,14 +1,12 @@
 package br.com.ovd.kafka.integration.model.source;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class ProductSite {
     private String codigoProduto;
     private String codigoSite;
--- src/main/java/br/com/ovd/kafka/integration/stream/transform/SearchEngineIntegrationProcessor.java
+++ src/main/java/br/com/ovd/kafka/integration/stream/transform/SearchEngineIntegrationProcessor.java
@@ -1,6 +1,10 @@
 package br.com.ovd.kafka.integration.stream.transform;
 
 import br.com.ovd.kafka.integration.model.ProductWithStocks;
+import br.com.ovd.kafka.integration.model.source.Product;
+import br.com.ovd.kafka.integration.model.source.ProductSite;
+import br.com.ovd.kafka.integration.model.source.Stock;
+import br.com.ovd.kafka.integration.model.source.StockList;
 import br.com.ovd.kafka.integration.stream.config.IntegrationConfig;
 import br.com.ovd.kafka.integration.stream.config.SiteConfig;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -9,7 +13,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.kafka.common.protocol.types.Field;
 import org.jboss.logging.Logger;
+
+import java.util.Optional;
 
 @Data
 @AllArgsConstructor
@@ -27,12 +34,46 @@ public class SearchEngineIntegrationProcessor {
     public void process(String key, ProductWithStocks value) {
 
         logger.info("searchEngineUrl = " + integrationConfig.searchEngineUrl());
-        System.out.println("searchEngineUrl = " + integrationConfig.searchEngineUrl());
 
         for (SiteConfig siteConfig: integrationConfig.sites()) {
             logger.info("Site " + siteConfig.site());
-            for (String org : siteConfig.orgs()) {
-                logger.info("Org " + org);
+            for (ProductSite productSite : value.getProduct().getSites()) {
+                Product product = value.getProduct();
+
+                Stock stock = null;
+
+                for (String sourceOrg : siteConfig.orgs()) {
+                    stock = value.getStocks()
+                            .getEstoques()
+                            .stream()
+                            .filter(s -> s.getFilial().equals(sourceOrg))
+                            .findFirst().get();
+                }
+                if (stock == null) {
+                    logger.info("Produto " + product.getModelo()
+                            + " não será integrado com a filial pois não consta na list de produtos.");
+                } else {
+
+                    String product_id = key;
+                    String apiKey = siteConfig.key();
+                    String secretKey = siteConfig.secret();
+                    String salesChannel = stock.getFilial();
+                    String status =
+                            (productSite
+                                    .getStatus()
+                                    .equalsIgnoreCase("inactive ")
+                                    ? "removed" : stock.getStatus());
+                    if (value.getProduct().isChanged()) {
+                        // TODO - PUT
+                        // preparar mensagem
+                        // enviar
+                        // escrever no log mensagem recebida
+                    } else {
+                        // TODO - POST
+                        // Enviar parametros gerais
+                        // escrever no log mensagem recebida
+                    }
+                }
             }
         }
     }
